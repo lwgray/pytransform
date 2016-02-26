5f1547583967042cdd451b514d07d4ccf5145e65
Larry Gray <larry.gray@nih.gov>
2016-02-25 21:55:15 -0500

    change mdanalysis version 0.8.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
     name = 'pytransform',
     packages = ['pytransform'],
-    version = '0.2.1',
+    version = '0.2.3',
     entry_points = {
         "console_scripts": ['transform = pytransform.transform:main']
         },
@@ -10,8 +10,8 @@ setup(
     author = 'Larry Gray',
     author_email = 'lwgray@gmail.com',
     url = 'https://github.com/lwgray/pyTransform',
-    download_url = 'https://github.com/lwgray/pyEntrezId/tarball/0.2.1',
+    download_url = 'https://github.com/lwgray/pyEntrezId/tarball/0.2.3',
     keywords = ['RNA', 'Molecular Structures', 'Transformation', '3d', 'Computational Biology'],
     classifiers = [],
-    install_requires=['MDAnalysis']
+    install_requires=['MDAnalysis==0.8.1']
 )
