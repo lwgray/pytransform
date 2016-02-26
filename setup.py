1138402004e05bc4cfdc56dedc996bb09d79bb7d
Larry Gray <larry.gray@nih.gov>
2016-02-25 21:08:31 -0500

    update setup

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup
 setup(
     name = 'pytransform',
     packages = ['pytransform'],
-    version = '0.2.0',
+    version = '0.2.1',
     entry_points = {
-        "console_scripts": ['pytransform = pytransform.pytransform:main']
+        "console_scripts": ['transform = pytransform.transform:main']
         },
     description = 'Applies transformation to structures and writes new pdb file',
     author = 'Larry Gray',
     author_email = 'lwgray@gmail.com',
     url = 'https://github.com/lwgray/pyTransform',
-    download_url = 'https://github.com/lwgray/pyEntrezId/tarball/0.2.0',
+    download_url = 'https://github.com/lwgray/pyEntrezId/tarball/0.2.1',
     keywords = ['RNA', 'Molecular Structures', 'Transformation', '3d', 'Computational Biology'],
     classifiers = [],
     install_requires=['MDAnalysis']
