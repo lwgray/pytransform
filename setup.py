5fc22782f7cc694814ee1f67a4b235115ebdd32f
Larry Gray <larry.gray@nih.gov>
2016-02-25 19:12:51 -0500

    update setup file

--- setup.py
+++ setup.py
@@ -13,10 +13,5 @@ setup(
     download_url = 'https://github.com/lwgray/pyEntrezId/tarball/0.1.0',
     keywords = ['RNA', 'Molecular Structures', 'Transformation', '3d', 'Computational Biology'],
     classifiers = [],
-    install_requires=['biopython==1.66', 'cffi==1.5.0', 'cryptography==1.2.1', 'decorator==4.0.9',
-    'ecdsa==0.13', 'enum34==1.1.2', 'fpconst==0.7.2', 'GridDataFormats==0.3.2', 'MDAnalysis==0.13.0',
-    'networkx==1.11', 'numpy==1.10.4', 'paramiko==1.16.0', 'ply==3.8', 'pycparser==2.14',
-    'pycrypto==2.6.1', 'pyOpenSSL==0.15.1', 'rsa==3.3', 'six==1.10.0', 'stevedore==1.10.0',
-    'virtualenv==14.0.6', 'virtualenv-clone==0.2.6', 'VirtualEnvOnDemand==4.1.0',
-    'virtualenvwrapper==4.7.1', 'wheel==0.29.0']
+    install_requires=['numpy==1.10.4', 'MDAnalysis==0.13.0']
 )
